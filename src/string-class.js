fa31775325f7b3c2aa122761c7e3b76ad9ec9a5b
Ologho Cyril Paul <cyrielo911@gmail.com>
2016-10-01 19:07:48 +0100

    Adds numberWords method

--- src/string-class.js
+++ src/string-class.js
@@ -91,3 +91,47 @@ String.prototype.getMiddle = function () {
   return middleCharacter;
 
 };
+
+String.prototype.numberWords = function () {
+  let word, str = '', char;
+  for (char = 0; char < this.length; char++) {
+    switch (this[char]){
+      case '1':
+        word = 'one';
+        break;
+      case '2':
+        word = 'two';
+        break;
+      case '3':
+        word = 'three';
+        break;
+      case '4':
+        word = 'four';
+        break;
+      case '5':
+        word = 'five';
+        break;
+      case '6':
+        word = 'six';
+        break;
+      case '7':
+        word = 'seven';
+        break;
+      case '8':
+        word = 'eight';
+        break;
+      case '9':
+        word = 'nine';
+        break;
+      case '0':
+        word = 'zero';
+        break;
+      default:
+        word = '';
+        break;
+    }
+    str += (word + ((char + 1) != this.length ? ' ' : ''));
+  }
+
+  return str;
+};
